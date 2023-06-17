871b3d7269e30cf5d3c4d65355b3feb202e2ea43
Phyo Thet Paing <sfcphyo31@gmail.com>
2023-06-17 16:05:02 +0630

    newUpdate

--- j4u-app/src/App.js
+++ j4u-app/src/App.js
@@ -56,6 +56,7 @@ function App() {
         <Route path="searchCompany" element={<SearchCompany />} />
 
         <Route path="about" element={<About />} />
+        <Route path="about" element={<About />} />
 
         <Route path="userTable" element={<UserTable />} />
 
---
+++ j4u-app/src/components/pages/FavoritePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FavoritePage = () => {
+  return (
+    <>
+    <h1>Fav</h1>
+    </>
+  )
+}
+
+export default FavoritePage
